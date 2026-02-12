4927c21aed7233fc6824b2d552bd3e6a50099dce
kirabelll <kirubelkassahun9@gmail.com>
2026-02-12 14:27:28 +0300

    commit

--- install-dokploy.sh
+++ install-dokploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "========================================="
-echo "   Dokploy Auto Installer - Ubuntu 24.04"
+echo "   Dokploy Auto Installer - Ubuntu Server"
 echo "========================================="
 
 # Update system
@@ -24,15 +24,11 @@ sudo apt remove -y docker docker-engine docker.io containerd runc || true
 echo "Installing Docker..."
 sudo install -m 0755 -d /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | 
-sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-echo 
-"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] 
-https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" 
-| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
 
